558f7c577c8f66efa397a4670499839fc8b480a4
SWYZZWH <843532380@qq.com>
2020-02-19 00:08:10 +0800

    switch to Chinese

--- actions.py
+++ actions.py
@@ -6,18 +6,41 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+from rasa_sdk import Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+from typing import Dict, Text, Any, List
+
+import requests
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet, FollowupAction
+from rasa_sdk.forms import FormAction
+
+base_url = "https://lab.isaaclin.cn/nCoV/api/"
+
+api_patterns = {
+    "overall":["latest"],
+    "provinceName":[],
+    "area":["latest","province"],
+    "news":["province","num"],
+    "rumors":["rumorType","num"]
+}
+
+#response_templates = {
+#    "current":"Temperature:{}\nCondition:{}",
+#    "someday":"Max Temperature:{}\nMin Temperature:{}\nCondition:{}\n"
+#}
+
+def create_url(api_pattern,paras):
+    url = base_url
+    if api_pattern in api_patterns.keys():
+        url += api_pattern
+        
+    if paras:
+        url += "?"
+        
+    url += "&".join([key+"="+value for key,value in paras.items()])
+    return url
 
 
 
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
--- data/stories.md
+++ data/stories.md
@@ -1,31 +1,19 @@
-## happy path
+## greet
 * greet
-  - utter_greet
-* mood_great
-  - utter_happy
+    - utter_greet
+    - utter_functions
 
-## sad path 1
-* greet
-  - utter_greet
-* mood_unhappy
-  - utter_cheer_up
-  - utter_did_that_help
-* affirm
-  - utter_happy
-
-## sad path 2
-* greet
-  - utter_greet
-* mood_unhappy
-  - utter_cheer_up
-  - utter_did_that_help
-* deny
-  - utter_goodbye
-
-## say goodbye
+## goodbye
 * goodbye
-  - utter_goodbye
+    - utter_goodbye
 
-## bot challenge
-* bot_challenge
-  - utter_iamabot
+## thankyou
+* thankyou
+    - utter_noworries
+
+## happy_path
+* greet
+    - utter_greet
+    - utter_functions
+* thankyou
+    - utter_noworries
